018e48c00e1b93072c8bb098b3909887c4240aff
GuangWuLin <1094210951@qq.com>
2016-12-09 17:21:37 +0800

    audio & sync

--- app/main.js
+++ app/main.js
@@ -1,12 +1,4 @@
-//import React from 'react';
-//require('rctui');
-//import ReactDOM from  'react-dom';
-// import { Router, Route, hashHistory} from "react-router";
 const React = require('react')
-const Router = require('react-router');
-const Route = require('react-router');
-const hashHistory = require('react-router');
-
 const electron = require('electron');
 // Module to control application life.
 const {app} = electron;
@@ -16,13 +8,38 @@ const {Menu, Tray} = require('electron')
 const MenuItem = electron.MenuItem
 const path = require('path')
 const ipcMain = require('electron').ipcMain;
-
+// 保持全局的窗口对象，可以不显示，如果没有这个对象，窗口点击关闭的时候，js对象会被gc干掉
 let win;
-let tray;
+var tray = null;
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({width: 800, height: 600});
+  // win 加载静态资源
   win.loadURL(`file://${__dirname}/index.html`);
+  // 创建一个平台图标
+  tray = new Tray(path.join(__dirname, '../thr/img/1.png'))
+  // 图标 菜单
+  const contextMenu = Menu.buildFromTemplate(
+      [
+        {label: '主界面',checked:true,click:()=>{win.webContents.send('HelloMan')}},
+        {label: '注销',
+          click:()=>{win.webContents.send('sayWhat')
+                    tray.setToolTip('消息助手')
+        }},
+        {label: '设置',click:()=>{win.webContents.send('userSetting')}},
+        {label: '退出',click:()=>{win.close()}} // 退出
+      ]
+    );
+  // 提示
+  tray.setToolTip('消息助手')
+  // 设置菜单到图标
+  tray.setContextMenu(contextMenu);
+  tray.on('balloon-click',()=>{
+    console.log(1111)
+  })
+  // console.log(tray)
+  //prepareAudio();
+
   // Open the DevTools.
   win.webContents.openDevTools();
   // Emitted when the window is closed.
@@ -30,6 +47,16 @@ function createWindow() {
     win = null;
   });
 }
+
+function prepareAudio(){
+    //var audio = new Audio('./audio/money.wav');
+    // tray.addEventListener('click',()=>{
+    //   audio.currentTime = 0;
+    //   audio.play();
+    // })
+    audio.play();
+  }
+
 app.on('ready', createWindow);
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -42,44 +69,14 @@ app.on('activate', () => {
   }
 });
 
-
-function createWindow() {
-  // Create the browser window.创建一个新的浏览器窗口
-  win = new BrowserWindow({width: 705, height: 525});
-  	
-  tray = new Tray(path.join(__dirname, '../thr/img/1.png'))
-
-  const contextMenu = Menu.buildFromTemplate(
-      [
-        {label: '主界面',checked:true,click:()=>{win.webContents.send('HelloMan')}},
-        {label: '注销',click:()=>{win.webContents.send('sayWhat')}},
-        {label: '设置',click:()=>{console.log(111111111)}},
-        {label: '退出',click:()=>{win.close()}} // 退出
-      ]
-    );
-
-  tray.setToolTip('消息助手（刘某）：暂无待办事项')
-  tray.setContextMenu(contextMenu);
-  // 并且装载应用的index.html
-  win.loadURL(`file://${__dirname}/index.html`);
-
-  // 打开开发者工具页面(屏蔽)
-  //win.webContents.openDevTools();
-
-  // 当窗口关闭时调用的方法
-  win.on('closed', () => {
-  		console.log('Hehe')
-      // 解除窗口对象的引用，通常而言如果应用支持多个窗口的话，你会在一个数组里
-      //存放窗口对象，在窗口关闭的时候
-      // 应当删除相应的元素。
-      win = null;
-  });
-}
-
 ipcMain.on('close-main-window',function(){
 	app.quit();
 })
-
-ipcMain.on('sayHi',function(){
-  console.log('Hi')
+ipcMain.on('CurrentUser',(event,msg)=>{
+  tray.displayBalloon({
+    icon:'../thr/img/2.jpg',
+    title:`Welcome ${msg[0]}`,
+    content:`There are ${msg[1].length} things exists `
+  });
+ tray.setToolTip(`消息助手 - 当前用户 ( ${msg[0]} ) 待办事件 ${msg[1].length}`)
 })
